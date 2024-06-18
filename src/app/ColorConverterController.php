f488538356f0c782889829baca2b094777cb6c15
justinwesterduin <justin.westerduin@maxserv.com>
2024-06-18 15:22:41 +0200

    first commit

---
+++ src/app/ColorConverterController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+class ColorConverterController
+{
+    public function inputCheck(): void
+    {
+        if (isset($_GET[ 'convert' ])) {
+
+            $red = $this->getColor(color: 'red');
+            $green = $this->getColor(color: 'green');
+            $blue = $this->getColor(color: 'blue');
+
+            if (empty($_GET[ 'rgb' ] | $red | $green| $blue)) {
+                header(header: 'index.php');
+            }
+
+            if($red > 255 | $green > 255 | $blue > 255) {
+                header(header: 'index.php');
+            }
+        }
+    }
+
+    private function getColor($color): string
+    {
+        if(isset($_GET[$color])) {
+            return (string)(int)$_GET[$color];
+        }
+        return '0';
+    }
+
+    public function rgbToHex(): string
+    {
+        $red = $this->getColor(color: 'red');
+        $green = $this->getColor(color: 'green');
+        $blue = $this->getColor(color: 'blue');
+        return sprintf("#%02x%02x%02x", $red, $green, $blue);
+    }
+}
---
+++ src/loader.php
@@ -0,0 +1,7 @@
+<?php
+
+$root = __DIR__ . '/public/';
+
+define('BASE_PATH', $root);
+
+require BASE_PATH . '../vendor/autoload.php';
